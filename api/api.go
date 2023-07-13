134818f92a5f9901d3522b9c4e8bd62e18540f13
soheil <soheilkhaledabdi@gmail.com>
2023-07-13 05:30:34 -0400

    register custom validation password

--- api/api.go
+++ api/api.go
@@ -2,17 +2,21 @@ package api
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/Asrez/GoAPIBlog/config"
 	"github.com/Asrez/GoAPIBlog/api/routers"
+	"github.com/Asrez/GoAPIBlog/api/validations"
+	"github.com/Asrez/GoAPIBlog/config"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 func InitServer(){
 	config := config.GetConfig()
 	r := gin.New()
 	r.Use(gin.Logger()  ,gin.Recovery())
-
+	RegisterValidators()
 	v1 := r.Group("api/v1")
 	{
 		auth := v1.Group("auth")
@@ -20,4 +24,16 @@ func InitServer(){
 	}
 
 	r.Run(fmt.Sprintf(":%s", config.Server.InternalPort))
+}
+
+
+func RegisterValidators() {
+	val, ok := binding.Validator.Engine().(*validator.Validate)
+	if ok {
+		err := val.RegisterValidation("password", validation.PasswordValidator, true)
+		if err != nil {
+			// logger.Error(logging.Validation, logging.Startup, err.Error(), nil)
+			log.Fatal("validation failed")
+		}
+	}
 }
