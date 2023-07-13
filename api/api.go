c5c05c357f551507be367b0d4accbf57023e53ed
soheil <soheilkhaledabdi@gmail.com>
2023-07-13 04:04:01 -0400

    add route auth

--- api/api.go
+++ api/api.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/Asrez/GoAPIBlog/config"
+	"github.com/Asrez/GoAPIBlog/api/routers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,10 +13,11 @@ func InitServer(){
 	r := gin.New()
 	r.Use(gin.Logger()  ,gin.Recovery())
 
-	// v1 := r.Group("api/v1")
-	// {
-		
-	// }
+	v1 := r.Group("api/v1")
+	{
+		auth := v1.Group("auth")
+		routers.Auth(auth , config)
+	}
 
 	r.Run(fmt.Sprintf(":%s", config.Server.InternalPort))
 }
